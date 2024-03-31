6063df00ac8add854921756aec75b7cb2c97c12c
Andre00785 <158030158+Andre00785@users.noreply.github.com>
2024-03-31 17:04:51 +0300

    3

--- game.js
+++ game.js
@@ -6,13 +6,15 @@ var config = {
         physics: {  
             default: 'arcade',
             arcade: {
-                gravity: { y: 200 }, 
+                gravity: { y: 700 }, 
                 debug: false
             }
         },
         preload: preload,
         create: create,
-        update: update
+        update: update,
+        shootBullet: shootBullet,
+        collectLives: collectLives
     }
 };
 
@@ -21,40 +23,68 @@ var worldWidth = 9600
 var console = console
 var platform
 var cursors
+var House
+var Angle
 
 function preload()
 {
-  this.load.image('Fon', 'assets/Fon1');
-  this.load.spriteship('dude', 'assets/Player',
+  this.load.image('Fon', 'assets/Fon1.png');
+  this.load.image('bullet', 'assets/Bullet.png');
+  this.load.image('001', 'assets/гідрант.png');
+  this.load.image('House', 'assets/house2.png');
+  this.load.image('ground', 'assets/platform2.png');
+  this.load.spritesheet('dude', 'assets/Player.png',
     { frameWidth: 147, frameHeight: 294 }
     );
 
-  this.load.spriteship('dude1', 'assets/Player1',
+    this.load.spritesheet('dude1', 'assets/Player1.png',
   { frameWidth: 147, frameHeight: 294 }
   );
+
+  this.load.spritesheet('Anon', 'assets/Anon.png',
+    { frameWidth: 147, frameHeight: 294 }
+    );
+
+    this.load.spritesheet('Anon1', 'assets/Anon1.png',
+    { frameWidth: 147, frameHeight: 294 }
+    );
 
 }
 
 function create()
 {
 
-  this.add.TileSprite(0, 0, worldWidth, 1080, "Fon")
+    cursors = this.input.keyboard.createCursorKeys();
+
+  this.add.tileSprite(0, 0, worldWidth, 1080, "Fon")
   .setOrigin(0,0)
   .setScale(1)
-  .setDepth(0);
+  .setDepth(-6);
 
-  player = this.physics.add.sprite(100, 450, 'dude');
-    player.setScale(1)
+  player = this.physics.add.sprite(100, 900, 'dude');
+    player.setScale(0.3)
+    player.setBounce(0.1)
+    player.setCollideWorldBounds(false)
+    player.setDepth(2);
 
-    player.setBounce(0.1);
-    player.setCollideWorldBounds(false);
+    platforms = this.physics.add.staticGroup();
 
 
+    this.input.on('pointerdown', shootBullet, this);
+
+
+    for (var x = 0; x < worldWidth; x = x + 128) {
+      console.log(x)
+      platforms.create(x, 1080 - 80, 'ground')
+          .setOrigin(0, 0)
+          .refreshBody()
+          .setScale(1);
+    };
 
     this.anims.create({ 
         key: 'left',
         frames: this.anims.generateFrameNumbers('dude1', { start: 3, end: 0 }),
-        frameRate: 10,
+        frameRate: 6.5,
         repeat: -1
     });
 
@@ -63,48 +93,159 @@ function create()
         frames: this.anims.generateFrameNumbers("dude", {
             frames: [0],
         }),
-        frameRate: 10,
+        frameRate: 6.5,
         repeat: -1
     });
 
     this.anims.create({
         key: 'right',
         frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 4 }),
-        frameRate: 10,
+        frameRate: 6.5,
         repeat: -1
     });
 
     player.body.setGravityY(50) 
 
     this.physics.add.collider(player, platforms);    
+
+
+    Anon = this.physics.add.sprite(5, 900, 'Anon');
+
+    Anon.setScale(0.3)
+    Anon.setBounce(0.1)
+    Anon.setCollideWorldBounds(false)
+    Anon.setDepth(-4);
+
+    this.anims.create({ 
+        key: 'l',
+        frames: this.anims.generateFrameNumbers('Anon1', { start: 3, end: 0 }),
+        frameRate: 6.5,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 't',
+        frames: this.anims.generateFrameNumbers("Anon", {
+            frames: [0],
+        }),
+        frameRate: 6.5,
+        repeat: -1
+    });
+
+    this.anims.create({
+        key: 'r',
+        frames: this.anims.generateFrameNumbers('Anon', { start: 0, end: 4 }),
+        frameRate: 6.5,
+        repeat: -1
+    });
+
+    this.physics.add.collider(Anon, platforms);
+
+
+    Anon1 = this.physics.add.sprite(9500, 900, 'Anon1');
+
+    Anon1.setScale(0.3)
+    Anon1.setBounce(0.1)
+    Anon1.setCollideWorldBounds(false)
+    Anon1.setDepth(-4);
+
+    this.physics.add.collider(Anon1, platforms);
+
+
+
+    House = this.physics.add.staticGroup();
+
+    for (var x = 0; x < worldWidth; x = x + 1000) {
+        console.log(x)
+        House.create(x, 1080 - 720, 'House')
+            .setOrigin(0, 0)
+            .refreshBody()
+            .setScale(1)
+            .setDepth(-5);
+    }
+
+
+    gidro = this.physics.add.staticGroup();
+
+    for (var x = 0; x < worldWidth; x = x + 500) {
+        console.log(x)
+        gidro.create(x, 1080 - 120, '001')
+            .setOrigin(0, 0)
+            .refreshBody()
+            .setScale(0.5)
+            .setDepth(-3);
+    }
+
   }
 
    
 
 function update()
 {
+
   this.cameras.main.setBounds(0, 0, worldWidth, window.innerHeight);
   this.physics.world.setBounds(0, 0, worldWidth, window.innerHeight);
   this.cameras.main.startFollow(player);
 
   if (cursors.left.isDown) 
-  {
+   {
       player.setVelocityX(-160);
 
-      player.anims.play('left', true);
-  }
+       player.anims.play('left', true);
+   }
   else if (cursors.right.isDown) {
       player.setVelocityX(160);
 
-      player.anims.play('right', true);
+       player.anims.play('right', true);
   }
-  else {
+   else {
       player.setVelocityX(0);
 
-      player.anims.play('turn');
-  }
+       player.anims.play('turn');
+   }
 
   if (cursors.up.isDown && player.body.touching.down) {
-      player.setVelocityY(-330);
+       player.setVelocityY(-330);
   }
+
+
+  if (cursors.down.isUp) 
+   {
+      Anon.setVelocityX(160);
+
+       Anon.anims.play('r', true);
+   }
+
+
+   if (cursors.down.isUp) 
+   {
+      Anon1.setVelocityX(-160);
+
+       Anon1.anims.play('l', true);
+   }
+
+}
+
+function shootBullet(pointer) {
+
+    let bullet = this.physics.add.sprite(player.x, playr.y, 'bullet')
+    .setScale(5)
+    .setDepth(2);
+
+
+    let angle = Phaser.Math.Angle.Between(player.x, playr.y, pointer.x, pointer.y);
+    let velocityX = Math.cos(angle) * 5000;
+    let velocityY = Math.sin(angle) * 500;
+
+    bullet.setVelocity(velocityX, velocityY);
+}
+
+function collectLives() {
+
+
+
+
+
+
+
 }
